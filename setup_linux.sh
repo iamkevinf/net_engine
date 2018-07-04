2292b2525b7e7fb6a3cb1a20fbcb197d1b63bc57
kevin <kevin@123.com>
2018-07-04 17:37:36 +0800

    fix g++

--- setup_linux.sh
+++ setup_linux.sh
@@ -3,5 +3,5 @@ g++ -c NetEngine/Src/client_socket.cpp -o build/client_socket.o
 
 g++ -c NetEngine/Src/tcp_client.cpp -o build/tcp_client.o
 
-g++ -std=c++11 NetEngine/EasyTcpServer/server.cpp -o bin/server -I NetEngine/Src build/tcp_server.o build/client_socket.o
+g++ -std=c++11 -pthread NetEngine/EasyTcpServer/server.cpp -o bin/server -I NetEngine/Src build/tcp_server.o build/client_socket.o
 g++ -std=c++11 -pthread NetEngine/EasyTcpClient/client.cpp -o bin/client -I NetEngine/Src build/tcp_client.o
