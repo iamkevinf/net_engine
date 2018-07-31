cfad05a7aa8bf12b51a39d9b38380206521384de
Kevin <kevin@123.com>
2018-07-31 00:29:21 -0400

    linux support

--- setup_linux.sh
+++ setup_linux.sh
@@ -4,11 +4,12 @@ g++ -c NetEngine/Src/cell.cpp -o build/cell.o
 g++ -c NetEngine/Src/cell_task.cpp -o build/cell_task.o
 
 g++ -std=c++11 -c NetEngine/Src/semaphore.cpp -o build/semaphore.o
+g++ -std=c++11 -c NetEngine/Src/cell_thread.cpp -o build/cell_thread.o
 
 g++ -c NetEngine/Src/my_server.cpp -o build/my_server.o
 g++ -c NetEngine/Src/net_time.cpp -o build/net_time.o
 
 g++ -c NetEngine/Src/tcp_client.cpp -o build/tcp_client.o
 
-g++ -g -std=c++11 -pthread NetEngine/EasyTcpServer/server.cpp -o bin/server -I NetEngine/Src build/my_server.o build/tcp_server.o build/client_socket.o build/net_time.o build/cell.o build/cell_task.o build/semaphore.o
+g++ -g -std=c++11 -pthread NetEngine/EasyTcpServer/server.cpp -o bin/server -I NetEngine/Src build/my_server.o build/tcp_server.o build/client_socket.o build/net_time.o build/cell.o build/cell_task.o build/semaphore.o build/cell_thread.o
 g++ -g -std=c++11 -pthread NetEngine/EasyTcpClient/client.cpp -o bin/client -I NetEngine/Src build/tcp_client.o build/net_time.o 
